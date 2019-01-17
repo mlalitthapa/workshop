f9cd0b0ba0eec7b050e1dc977b327421af22a372
Lalit Thapa <mlalitthapa@gmail.com>
2019-01-17 21:58:14 +0545

    switch condition

--- 04_conditions.go
+++ 04_conditions.go
@@ -4,12 +4,24 @@ import "fmt"
 
 func main() {
 	ifCondition(55)
+	switchCondtion("foo")
 }
 
 func ifCondition(num int) {
-	if num % 2 == 0 {
+	if num%2 == 0 {
 		fmt.Printf("%d is even \n", num)
 	} else {
 		fmt.Printf("%d is odd \n", num)
+	}
+}
+
+func switchCondtion(pair string) {
+	switch pair {
+	case "foo":
+		fmt.Printf("%s -> bar", pair)
+	case "john":
+		fmt.Printf("%s -> doe", pair)
+	default:
+		fmt.Printf("%s -> :(", pair)
 	}
 }
