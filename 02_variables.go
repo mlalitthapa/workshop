40816748c6dda89ef117b7c9cfa3bac661332ce6
Lalit Thapa <mlalitthapa@gmail.com>
2019-01-17 21:36:45 +0545

    types of variables

---
+++ 02_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	var b = "string"
+
+	fmt.Printf("a = %s \nb = %s \n", a, b)
+
+	x := 1
+	y := 0.001
+	fmt.Printf("x = %d\ny = %.3f", x, y)
+}
