3d74e4d3c9c1e656ef5d14408c52e8a461ebd548
Lalit Thapa <lalit@ekbana.com>
2019-01-18 10:11:55 +0545

    callback functions

--- functions.go
+++ functions.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	total := sum(1, 2, 3, 4, 5)
-	fmt.Println(total)
+	sum := sum(1, 2, 3, 4, 5)
+	fmt.Println(sum)
+
+	nums := []int{2, 5, 7, 9, 15}
+	average := average(nums, total)
+	fmt.Println(average)
 }
 
 // Variadic function
@@ -14,4 +18,17 @@ func sum(args ...int) int {
 		sum += n
 	}
 	return sum
+}
+
+func total(nums []int) int {
+	var sum int
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
+}
+
+func average(nums []int, cb func([]int) int) float64 {
+	total := cb(nums)
+	return float64(total) / float64(len(nums))
 }
